0b86d3cefaa6ebc65d9923e7e15362179782cf19
andresalvs <andresalvadorgregorio@gmail.com>
2024-11-11 23:27:12 +0800

    add hov

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -1,7 +1,6 @@
-import React, {useEffect} from 'react';
-import classNames from 'classnames';
+import React, { useEffect } from 'react';
 import Image from 'next/image';
-import {portfolioItems, SectionId} from '../../data/data';
+import { portfolioItems, SectionId } from '../../data/data';
 import Section from '../Layout/Section';
 
 const Portfolio: React.FC = React.memo(() => {
@@ -43,15 +42,19 @@ const Portfolio: React.FC = React.memo(() => {
         </h2>
         <div className="w-full columns-2 md:columns-3 lg:columns-4">
           {portfolioItems.map((item, index) => {
-            const {title, image} = item;
+            const { title, image, url } = item;
             return (
               <div className="portfolio-item pb-6" key={`${title}-${index}`}>
-                <div
-                  className={classNames(
-                    'relative w-full h-max overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl',
-                  )}>
-                  <Image alt={title} className="w-full h-full" placeholder="blur" src={image} />
-                </div>
+                <a
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="block w-full h-max overflow-hidden rounded-lg shadow-lg shadow-black/30 lg:shadow-xl"
+                >
+                  <div className="relative w-full h-full">
+                    <Image alt={title} className="w-full h-full" placeholder="blur" src={image} />
+                  </div>
+                </a>
               </div>
             );
           })}
